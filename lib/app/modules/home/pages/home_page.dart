b70dde50e9f7f5cdeafe15efa45c9b0edb20f3bc
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-08-01 23:00:10 -0300

    feat - implemet speech-to-text

--- lib/app/modules/home/pages/home_page.dart
+++ lib/app/modules/home/pages/home_page.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import '../stores/home_store.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+// import 'package:speech_to_text/speech_to_text.dart' as speechToText;
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -12,28 +16,91 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   final HomeStore homeStore = Modular.get();
-
   late TabController _tabController;
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+  // late speechToText.SpeechToText speech;
+  // String textString = "Press The Button";
+  // bool isListen = false;
+  // double confidence = 1.0;
+
+  // void listenVoice() async {
+  //   if (!isListen) {
+  //     bool avail = await speech.initialize();
+  //     if (avail) {
+  //       setState(() {
+  //         isListen = true;
+  //       });
+  //       speech.listen(onResult: (value) {
+  //         setState(() {
+  //           textString = value.recognizedWords;
+  //           if (value.hasConfidenceRating && value.confidence > 0) {
+  //             confidence = value.confidence;
+  //           }
+  //         });
+  //       });
+  //     }
+  //   } else {
+  //     setState(() {
+  //       isListen = false;
+  //     });
+  //     speech.stop();
+  //   }
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+    // speech = speechToText.SpeechToText();
+    _tabController = TabController(
+      length: _kTabPages.length,
+      vsync: this,
+    );
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+      print(_lastWords);
+    });
+  }
 
   final _kTabPages = <Widget>[
+    // ignore: prefer_const_constructors
     Padding(
       padding: const EdgeInsets.only(top: 250.0),
-      child: Column(
-        children: <Widget>[
-          Image.asset(
-            "lib/app/modules/home/assets/homePage/speak.png",
-            width: 200,
-            height: 200,
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Text('Toque para falar',
-                  style: TextStyle(fontSize: 14, color: Colors.black)),
-            ),
-          ),
-        ],
+      child: const Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: Text('Toque para falar',
+              style: TextStyle(fontSize: 14, color: Colors.black)),
+        ),
       ),
     ),
     const Center(child: Icon(Icons.alarm, size: 64.0, color: Colors.cyan)),
@@ -44,21 +111,6 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     Tab(icon: Icon(Icons.note), text: 'Lembretes'),
     Tab(icon: Icon(Icons.view_agenda), text: 'Agenda'),
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      length: _kTabPages.length,
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +126,21 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           controller: _tabController,
         ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 235.0),
+        child: FlatButton(
+          onPressed:
+              // If not yet listening for speech start, otherwise stop
+              _speechToText.isNotListening ? _startListening : _stopListening,
+          // tooltip: 'Listen',
+          child: Image.asset(
+            "lib/app/modules/home/assets/homePage/speak.png",
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
