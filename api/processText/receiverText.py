fbc1be7dfe539954dc97c447e27ae4e10af7ad7f
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-08-10 18:39:25 -0300

    feat - structure class

--- api/processText/receiverText.py
+++ api/processText/receiverText.py
@@ -4,9 +4,9 @@ class ProcessText():
     def __init__(self, text):
         self.text = text
 
-    def processReceiver(self):
+    def processReceiver(self) -> list:
         instance = self.text
-        # print(instance)
-        # teste = 'chupa consegui'
-        # return [{"id": 694,"body": teste}]
-        return instance
+        instanceSplitText = str(instance).split(',')
+        instanceSplitid = instanceSplitText[1].split(')')  #String
+        text = instanceSplitText[3].split(')])]')          #String
+        return [{"id": int(instanceSplitid[0]),"body": text[0]}]
