4fd67b3be595690bdb473d6508d0f93b811e3095
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-08-10 19:18:46 -0300

    feat - create method tokenize in api

--- api/processText/receiverText.py
+++ api/processText/receiverText.py
@@ -1,12 +1,17 @@
 from cgitb import text
+from nltk.tokenize import word_tokenize
 
 class ProcessText():
     def __init__(self, text):
         self.text = text
 
-    def processReceiver(self) -> list:
+    def processReceiver(self):
         instance = self.text
+        print(self.tokenize())
         instanceSplitText = str(instance).split(',')
         instanceSplitid = instanceSplitText[1].split(')')  #String
         text = instanceSplitText[3].split(')])]')          #String
         return [{"id": int(instanceSplitid[0]),"body": text[0]}]
+
+    def tokenize(self):
+        return word_tokenize(str(self.text))
