2c034cc9f43ba8ba181e2279f2581e81461ce57e
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-08-12 18:27:13 -0300

    feat - pre and pro process

--- api/processText/receiverText.py
+++ api/processText/receiverText.py
@@ -1,5 +1,6 @@
 from cgitb import text
 from nltk.tokenize import word_tokenize
+import re
 
 class ProcessText():
     def __init__(self, text):
@@ -8,15 +9,35 @@ class ProcessText():
     def processReceiver(self):
         instance = self.text
         instanceSplitText = str(instance).split(',')
-        instanceSplitid = instanceSplitText[1].split(')')  #String
-        text = instanceSplitText[3].split(')])]')          #String
-        # print(self.tokenize(text))
-        # return [{"id": int(instanceSplitid[0]),"body": text[0]}]
+        instanceSplitid = instanceSplitText[1].split(')')  
+        text = instanceSplitText[3].split(')])]')         
         return [{"id": int(instanceSplitid[0]),"body": self.tokenize(text)}]
 
-    def tokenize(self, variable):
-        openText = word_tokenize(str(variable))
+    def preProcess(self, variable):
+        newList = []
+        lista = variable
+        for x in lista:
+            item = x
+            for y in ['[', ']', ',', '.', '``', "''", "'"]:
+                item = item.replace(y, "")
+            newList.append(item)
+        return newList
+
+    def proProcessTokenList(self, openText):
+        TokenList = []
         for i in openText:
+            if i != "[" and i != "]" and i != "``" and i != "'" and i != "''" and i != "," and '"' and "'":
+                TokenList.append(i)
+        return TokenList
+    
+    def tokenize(self, variable):
+        preClean = self.preProcess(variable)
+        openText = word_tokenize(str(preClean))
+        resultProProcess = self.proProcessTokenList(openText)
+        print(resultProProcess)
+        for i in resultProProcess:
             if i == 'are':
                 return 'passed'
         return variable[0]
+
+
