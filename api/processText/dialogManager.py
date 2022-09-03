93450ca20f9db2bee7ddef3f654651fec7278223
Andre Costa Silva <andreshippuden3711@gmail.com>
2022-09-03 11:06:24 -0300

    update - dialogs and dialogManager

--- api/processText/dialogManager.py
+++ api/processText/dialogManager.py
@@ -1,20 +1,50 @@
 from processText.dialogs import Dialog
 
-
 class DialogManager():
     def __init__(self):
         self.burden = 0
         self.type = 'user' 
         self.local = 'quarto'
+        self.ticket = ''
+
+        self.medicineList = []
+        self.shoppingList = []
+        
+        self.events = []
+        self.hoursEvents = []
 
     def dialogPolicy(self, textList, variable):
+        print(textList)
         try:
             for i in textList:
+                #Callbacks Dialogs Sensors
                 if self.type == 'smoke_detector':
                     self.burden += 1
+                if self.type == 'smartwach_fall_user':
+                    self.burden += 1
+                if self.type == 'smartwach_hight_heart_hate':
+                    self.burden += 1
+                if self.type == 'smartwach_low_beats':
+                    self.burden += 1
+                if self.type == 'sensor_presence':
+                    self.burden += 1
+
+                #Callbacks Dialogs for Sensors Users
                 elif self.type == 'user' and i == 'incêndio':
                     self.burden += 2
-            return Dialog().dialogReturn(self.burden, self.type, self.local)       
+                    self.ticket = 'incêndio'
+                elif self.type == 'user' and i == 'caí':
+                    self.burden += 2     
+                    self.ticket = 'caí'             
+                elif self.type == 'user' and i == 'sentindo': 
+                    self.burden += 2
+                    self.ticket = 'sentindo'     
+                elif self.type == 'user' and i == 'luz':
+                    self.burden += 2
+                    self.ticket = 'luz' 
+
+                #TODO Tem que fazer com User pois encadea vários dialogos
+            return Dialog().dialogReturn(self.burden, self.type, self.local, self.ticket)       
         except:
             return variable[0]
     
--- api/processText/dialogs.py
+++ api/processText/dialogs.py
@@ -1,7 +1,26 @@
 class Dialog():
-    def dialogReturn(self, burden, type, local):
+    def dialogReturn(self, burden, type, local, ticket):
+
+        #Dialogs Sensors
         if type == 'smoke_detector' and burden == 1:
             return  'Foi detectado um incêndio acontecendo no '+ local + ', por favor se dirigir para fora da casa. O corpo de bombeiros ja foi acionado'
-        if type == 'user' and burden == 2:
+        if type == 'smartwach_fall_user' and burden == 1:
+            return  'Foi detectado sua queda, por precaução a ambulância foi chamada'
+        if type == 'smartwach_hight_heart_hate' and burden == 1:
+            return  'Foi detectado que sua frequência cardíaca está alta, por precaução a ambulância foi chamada'
+        if type == 'smartwach_low_beats' and burden == 1:
+            return  'Foi detectado que sua frequência cardíaca está baixa, por precaução a ambulância foi chamada'
+        if type == 'sensor_presence' and burden == 1:
+            return  'luz ligada'
+
+        #Dialogs for Sensors Users
+        if type == 'user' and burden == 2 and ticket == 'incêndio':
             return  'Por favor se dirigir para fora da casa, o corpo de bombeiros ja foi acionado'
+        if type == 'user' and burden == 2 and ticket == 'caí':
+            return  'Ok, por precaução a ambulância foi chamada'
+        if type == 'user' and burden == 2 and ticket == 'sentindo':
+            return  'Ok, por precaução a ambulância foi chamada'
+        if type == 'user' and burden == 2 and ticket == 'luz':
+            return  'Ok, a luz foi ligada'
+
         return 'não compreendi, poderia falar novamente'
